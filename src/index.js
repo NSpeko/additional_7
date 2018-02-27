cea8fe6fc8971573a4cdf688119fe4d14f13bff5
NSpeko <nonstoper42@gmail.com>
2018-02-27 22:54:03 +0300

    Feature: Found weird solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+    // your solution
+    for (let x = 0; x < 9; x++) {
+        for (let y = 0; y < 9; y++) {
+            if (matrix[x][y] === 0) {
+                let result = [];
+                for (let i = 0; i < 9; i++) {
+                    result.push([matrix[x][i], matrix[i][y], matrix[Math.floor(x / 3) * 3 + i % 3]
+                        [Math.floor(y / 3) * 3 + Math.floor(i / 3)]])
+                }
+                for (let i=0;i<result.length;i++) {
+                    matrix[x][y] = result[i];
+                    solveSudoku(matrix);
+                }
+            }
+        }
+    }
+    return matrix;
 }
